d0d6a3775a43ea475d79280670dcd2f15062a07a
Megan Meiser <mmeiser19@gmail.com>
2022-02-11 20:50:15 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company; //comment this out when submitting
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String filename = args[0];
+    }
+    public static int add(String s1, String s2) {
+        return 0;
+    }
+
+    public static int multiply(String s1, String s2) {
+        return 0;
+    }
+
+    public static int exponentiate(String s1, String s2) {
+        return 0;
+    }
+}
